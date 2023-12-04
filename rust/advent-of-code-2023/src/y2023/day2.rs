cdd06eaec5cbdda6e662ae7a00b3e789a875e6b6
azthec <a.godspeed@gmail.com>
2023-12-04 17:12:45 +0100

    #aoc2023 day2 part2

--- rust/advent-of-code-2023/src/y2023/day2.rs
+++ rust/advent-of-code-2023/src/y2023/day2.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct Game {
   id: u32,
   cubes: Vec<DrawnCubes>,
@@ -12,6 +13,7 @@ impl Game {
   }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct DrawnCubes {
   red: u32,
   blue: u32,
@@ -79,9 +81,22 @@ pub fn part1(games: &Vec<Game>) -> u32 {
     .sum()
 }
 
+fn min_cubes(game: &Game) -> (u32, u32, u32) {
+  game.cubes.iter().fold(
+    (0, 0, 0),
+    |(min_red, min_blue, min_green), drawn_cubes| {
+      (
+        min_red.max(drawn_cubes.red),
+        min_blue.max(drawn_cubes.blue),
+        min_green.max(drawn_cubes.green),
+      )
+    },
+  )
+}
+
 #[aoc(day2, part2)]
 pub fn part2(games: &Vec<Game>) -> u32 {
-  todo!()
+  games.iter().map(min_cubes).map(|(r, g, b)| r * g * b).sum()
 }
 
 #[cfg(test)]
@@ -89,13 +104,210 @@ mod tests {
   use super::*;
 
   #[test]
-  fn part1_example() {
-    assert_eq!(part1(&parse("TODO")), 2);
+  fn parser_sample1() {
+    assert_eq!(
+      parse("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+      vec![Game {
+        id: 1,
+        cubes: vec![
+          DrawnCubes {
+            red: 4,
+            blue: 3,
+            green: 0
+          },
+          DrawnCubes {
+            red: 1,
+            blue: 6,
+            green: 2
+          },
+          DrawnCubes {
+            red: 0,
+            blue: 0,
+            green: 2
+          }
+        ]
+      }]
+    );
   }
 
   #[test]
-  fn part2_example() {
-    assert_eq!(part2(&parse("TODO")), 2);
+  fn parser_sample2() {
+    assert_eq!(
+      parse("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+      vec![Game {
+        id: 2,
+        cubes: vec![
+          DrawnCubes {
+            red: 0,
+            blue: 1,
+            green: 2
+          },
+          DrawnCubes {
+            red: 1,
+            blue: 4,
+            green: 3
+          },
+          DrawnCubes {
+            red: 0,
+            blue: 1,
+            green: 1
+          }
+        ]
+      }]
+    );
+  }
+
+  #[test]
+  fn parser_sample3() {
+    assert_eq!(
+      parse("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
+      vec![Game {
+        id: 3,
+        cubes: vec![
+          DrawnCubes {
+            red: 20,
+            blue: 6,
+            green: 8
+          },
+          DrawnCubes {
+            red: 4,
+            blue: 5,
+            green: 13
+          },
+          DrawnCubes {
+            red: 1,
+            blue: 0,
+            green: 5
+          }
+        ]
+      }]
+    );
+  }
+
+  #[test]
+  fn parser_sample4() {
+    assert_eq!(
+      parse("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
+      vec![Game {
+        id: 4,
+        cubes: vec![
+          DrawnCubes {
+            red: 3,
+            blue: 6,
+            green: 1
+          },
+          DrawnCubes {
+            red: 6,
+            blue: 0,
+            green: 3
+          },
+          DrawnCubes {
+            red: 14,
+            blue: 15,
+            green: 3
+          }
+        ]
+      }]
+    );
+  }
+
+  #[test]
+  fn parser_sample5() {
+    assert_eq!(
+      parse("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
+      vec![Game {
+        id: 5,
+        cubes: vec![
+          DrawnCubes {
+            red: 6,
+            blue: 1,
+            green: 3
+          },
+          DrawnCubes {
+            red: 1,
+            blue: 2,
+            green: 2
+          }
+        ]
+      }]
+    );
+  }
+
+  #[test]
+  fn test_min_cubes() {
+    let game = Game {
+      id: 1,
+      cubes: vec![
+        DrawnCubes {
+          red: 3,
+          blue: 4,
+          green: 0,
+        },
+        DrawnCubes {
+          red: 1,
+          blue: 2,
+          green: 6,
+        },
+        DrawnCubes {
+          red: 0,
+          blue: 0,
+          green: 2,
+        },
+      ],
+    };
+
+    assert_eq!(min_cubes(&game), (0, 0, 0));
+  }
+
+  #[test]
+  fn test_validate1() {
+    let game = Game {
+      id: 1,
+      cubes: vec![
+        DrawnCubes {
+          red: 3,
+          blue: 4,
+          green: 0,
+        },
+        DrawnCubes {
+          red: 1,
+          blue: 2,
+          green: 6,
+        },
+        DrawnCubes {
+          red: 0,
+          blue: 0,
+          green: 2,
+        },
+      ],
+    };
+
+    assert!(game.validate(5, 7, 5));
+  }
+
+  #[test]
+  fn test_validate2() {
+    let game = Game {
+      id: 1,
+      cubes: vec![
+        DrawnCubes {
+          red: 3,
+          blue: 4,
+          green: 0,
+        },
+        DrawnCubes {
+          red: 1,
+          blue: 2,
+          green: 6,
+        },
+        DrawnCubes {
+          red: 0,
+          blue: 0,
+          green: 2,
+        },
+      ],
+    };
+
+    assert!(!game.validate(5, 5, 5));
   }
 }
-
