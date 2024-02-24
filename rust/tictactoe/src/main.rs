bcca51ade656e035e505f276f447e3c3e4a5eae4
azthec <a.godspeed@gmail.com>
2024-02-24 19:34:24 +0100

    #tictactoe example structure

--- rust/tictactoe/src/main.rs
+++ rust/tictactoe/src/main.rs
@@ -1,55 +1,85 @@
+use anyhow::Result;
 use crossterm::{
-  event::{self, KeyCode, KeyEventKind},
+  event::{self, Event::Key, KeyCode::Char},
   execute,
   terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-  ExecutableCommand,
 };
 use ratatui::{
-  prelude::{CrosstermBackend, Stylize, Terminal},
+  prelude::{CrosstermBackend, Frame, Terminal},
+  style::Stylize,
   widgets::Paragraph,
 };
-
-use std::io::Result;
 use std::io::{stderr, stdout};
 
-fn main() -> Result<()> {
+struct State {
+  counter: i64,
+  quit: bool,
+}
+
+fn startup() -> Result<()> {
   enable_raw_mode()?;
   execute!(stderr(), EnterAlternateScreen)?;
-  let mut terminal = Terminal::new(CrosstermBackend::new(stderr()))?;
-  terminal.clear()?;
+  Ok(())
+}
 
-  // mutable state
-  let mut counter = 0;
+fn shutdown() -> Result<()> {
+  execute!(stderr(), LeaveAlternateScreen)?;
+  disable_raw_mode()?;
+  Ok(())
+}
 
-  loop {
-    // draw
-    let _ = terminal.draw(|frame| {
-      let area = frame.size();
-      frame.render_widget(
-        Paragraph::new(format!("Counter: {counter}"))
-          .white()
-          .on_blue(),
-        area,
-      );
-    })?;
+fn draw(state: &State, frame: &mut Frame) {
+  frame.render_widget(
+    Paragraph::new(format!("Counter: {0}", state.counter))
+      .white()
+      .on_blue(),
+    frame.size(),
+  )
+}
 
-    // handle event
-    if event::poll(std::time::Duration::from_millis(16))? {
-      if let event::Event::Key(key) = event::read()? {
-        if key.kind == crossterm::event::KeyEventKind::Press {
-          match key.code {
-            crossterm::event::KeyCode::Char('j') => counter += 1,
-            crossterm::event::KeyCode::Char('k') => counter -= 1,
-            crossterm::event::KeyCode::Char('q') => break,
-            _ => {}
-          }
+fn update(state: &mut State) -> Result<()> {
+  if event::poll(std::time::Duration::from_millis(16))? {
+    if let Key(key) = event::read()? {
+      if key.kind == event::KeyEventKind::Press {
+        match key.code {
+          Char('j') => state.counter += 1,
+          Char('k') => state.counter -= 1,
+          Char('q') => state.quit = true,
+          _ => {}
         }
       }
     }
   }
+  Ok(())
+}
 
-  execute!(stderr(), LeaveAlternateScreen)?;
-  disable_raw_mode()?;
+fn run() -> Result<()> {
+  let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
 
+  let mut state = State {
+    counter: 0,
+    quit: false,
+  };
+
+  loop {
+    t.draw(|f| {
+      draw(&state, f);
+    })?;
+
+    update(&mut state)?;
+
+    if state.quit {
+      break;
+    }
+  }
+
+  Ok(())
+}
+
+fn main() -> Result<()> {
+  startup()?;
+  let status = run();
+  shutdown()?;
+  status?;
   Ok(())
 }
