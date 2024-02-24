126c9201c905c01489fac523965da215e4fc86fc
azthec <a.godspeed@gmail.com>
2024-02-24 19:17:24 +0100

    #tictactoe example counter

--- rust/tictactoe/src/main.rs
+++ rust/tictactoe/src/main.rs
@@ -1,29 +1,32 @@
 use crossterm::{
   event::{self, KeyCode, KeyEventKind},
+  execute,
   terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
   ExecutableCommand,
 };
-
 use ratatui::{
   prelude::{CrosstermBackend, Stylize, Terminal},
-  terminal,
   widgets::Paragraph,
 };
-use std::io::stdout;
+
 use std::io::Result;
+use std::io::{stderr, stdout};
 
 fn main() -> Result<()> {
-  stdout().execute(EnterAlternateScreen)?;
   enable_raw_mode()?;
-  let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+  execute!(stderr(), EnterAlternateScreen)?;
+  let mut terminal = Terminal::new(CrosstermBackend::new(stderr()))?;
   terminal.clear()?;
+
+  // mutable state
+  let mut counter = 0;
 
   loop {
     // draw
     let _ = terminal.draw(|frame| {
       let area = frame.size();
       frame.render_widget(
-        Paragraph::new("Hello, welcome to ratatui, press q to quit!")
+        Paragraph::new(format!("Counter: {counter}"))
           .white()
           .on_blue(),
         area,
@@ -33,13 +36,20 @@ fn main() -> Result<()> {
     // handle event
     if event::poll(std::time::Duration::from_millis(16))? {
       if let event::Event::Key(key) = event::read()? {
-        if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-          break;
+        if key.kind == crossterm::event::KeyEventKind::Press {
+          match key.code {
+            crossterm::event::KeyCode::Char('j') => counter += 1,
+            crossterm::event::KeyCode::Char('k') => counter -= 1,
+            crossterm::event::KeyCode::Char('q') => break,
+            _ => {}
+          }
         }
       }
     }
   }
-  stdout().execute(LeaveAlternateScreen)?;
+
+  execute!(stderr(), LeaveAlternateScreen)?;
   disable_raw_mode()?;
+
   Ok(())
 }
