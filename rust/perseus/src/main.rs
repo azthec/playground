362f3fd01e15b2ef671b41b18fc284a050ff8d14
azthec <azthec@users.noreply.github.com>
2024-07-21 13:35:32 +0200

    #rust perseus add tailwindcss

--- rust/perseus/src/main.rs
+++ rust/perseus/src/main.rs
@@ -1,11 +1,22 @@
+mod error_views;
 mod templates;
 
-use perseus::prelude::*;
+use perseus::{plugins::Plugins, prelude::*};
 
 #[perseus::main(perseus_axum::dflt_server)]
 pub fn main<G: Html>() -> PerseusApp<G> {
   PerseusApp::new()
+    .plugins(Plugins::new().plugin(
+      perseus_tailwind::get_tailwind_plugin,
+      perseus_tailwind::TailwindOptions {
+        in_file: Some("src/tailwind.css".into()),
+        out_file: "dist/tailwind.css".into(),
+        config_path: None,
+      },
+    ))
+    .static_alias("/tailwind.css", "dist/tailwind.css")
     .template(crate::templates::index::get_template())
     .template(crate::templates::about::get_template())
-    // .template(crate::templates::new_index::get_template())
+    .template(crate::templates::new_index::get_template())
+    .error_views(crate::error_views::get_error_views())
 }
--- rust/perseus/src/templates/index.rs
+++ rust/perseus/src/templates/index.rs
@@ -3,16 +3,22 @@ use sycamore::prelude::*;
 
 fn index_page<G: Html>(cx: Scope) -> View<G> {
   view! { cx,
-      // Don't worry, there are much better ways of styling in Perseus!
-      div(style = "display: flex; flex-direction: column; justify-content: center; align-items: center; height: 95vh;") {
-          h1 { "Welcome to Perseus!" }
-          p {
-              "This is just an example app. Try changing some code inside "
-              code { "src/templates/index.rs" }
-              " and you'll be able to see the results here!"
-          }
-          a(href = "about", id = "about-link") { "About!" }
+    div(class = "flex min-h-full flex-col justify-center px-6 py-12 lg:px-8") {
+      div(class = "sm:mx-auto sm:w-full sm:max-w-sm") {
+        h1(class = "mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900") {
+          "Welcome to perseus!"
+        }
+        p(class="mt-1 max-w-2xl text-center text-sm leading-6 text-gray-500") {
+          "This is just an example app. Try changing some code inside "
+          code { "src/templates/index.rs" }
+          " and you'll be able to see the results here!"
+        }
+        a(href = "about", id = "about-link",
+            class = "inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10") {
+          "About!"
+        }
       }
+    }
   }
 }
 
@@ -20,6 +26,7 @@ fn index_page<G: Html>(cx: Scope) -> View<G> {
 fn head(cx: Scope) -> View<SsrNode> {
   view! { cx,
       title { "Welcome to Perseus!" }
+      link(href="./tailwind.css", rel="stylesheet") {}
   }
 }
 
